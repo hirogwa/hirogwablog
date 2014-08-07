d752207b1e899d28a3f85612fa949c77f76ddf1e
hirogwa <hirogwa@gmail.com>
2014-08-06 23:30:57 -0700

    add tag feature. #7

--- admin.py
+++ admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from models import Entry, Blog, Category, Comment, Theme
+from models import Entry, Blog, Category, Comment, Theme, Tag
 
 # Register your models here.
-admin.site.register(Theme)
 admin.site.register(Blog)
-admin.site.register(Entry)
 admin.site.register(Category)
 admin.site.register(Comment)
+admin.site.register(Entry)
+admin.site.register(Tag)
+admin.site.register(Theme)
